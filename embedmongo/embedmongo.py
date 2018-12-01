1129f2c1a87a961442766122db0053f973e8af6d
Karol Horowski <karol.hor@gmail.com>
2018-12-01 15:51:39 +0100

    add missing licesne headers

--- embedmongo/embedmongo.py
+++ embedmongo/embedmongo.py
@@ -12,7 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import logging
-logging.getLogger(__name__).addHandler(logging.NullHandler())
 
-__version__ = '0.1.0'
+class EmbedMongo:
+    pass
